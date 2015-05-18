0b7b38f3ce52dcad018e3425282c055f47f123f8
swjones <swjones@uvic.ca>
2015-05-18 10:49:13 +0200

    switching back to fherwig repo

--- run-ipython-notebook.bash
+++ run-ipython-notebook.bash
@@ -1,11 +1,15 @@
 #!/bin/bash                                                                     
 export PYTHONPATH=/home/nugrid
 
-IPN_URL="https://github.com/swjones/nugridnotebooks.git"
+#IPN_URL="https://github.com/swjones/nugridnotebooks.git"
+IPN_URL="https://github.com/fherwig/nugridnotebooks.git"
 IPN_LOCAL="/home/nugrid/nugridnotebooks"
 IPN_DIR="${IPN_LOCAL}/notebooks"
 
 [[ -d ${IPN_DIR} ]] || git clone ${IPN_URL} ${IPN_LOCAL}
+
+# add Luke's widget module to python path:
+export PYTHONPATH=${IPN_LOCAL}/modules:$PYTHONPATH
 
 ipython notebook \
     --profile=nbserver \
