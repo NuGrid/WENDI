865dd1fbdaa17632431ab184812e0b591b5fa560
Luke Siemens <lsiemens@users.noreply.github.com>
2015-05-19 23:56:49 -0700

    Added OMEGA_SYGMA

--- run-ipython-notebook.bash
+++ run-ipython-notebook.bash
@@ -6,10 +6,14 @@ IPN_URL="https://github.com/fherwig/nugridnotebooks.git"
 IPN_LOCAL="/home/nugrid/nugridnotebooks"
 IPN_DIR="${IPN_LOCAL}/notebooks"
 
+OMEGA_SYGMA_URL="https://bitbucket.com/critter1/omega_sygma.git"
+OMEGA_SYGMA_DIR="/home/nugrid/omega_sygma"
+
 [[ -d ${IPN_DIR} ]] || git clone ${IPN_URL} ${IPN_LOCAL}
+[[ -d ${OMEGA_SYGMA_DIR} ]] || git clone ${OMEGA_SYGMA_URL} ${OMEGA_SYGMA_DIR}
 
 # add Luke's widget module to python path:
-export PYTHONPATH=${IPN_LOCAL}/modules:$PYTHONPATH
+export PYTHONPATH=${IPN_LOCAL}/modules:${OMEGA_SYGMA_DIR}:$PYTHONPATH
 
 ipython notebook \
     --profile=nbserver \
